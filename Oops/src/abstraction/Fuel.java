448b4ea2766881c23645b3cd5589793988a22a43
jackiran9009 <104552005+jackiran9009@users.noreply.github.com>
2022-04-28 12:33:05 +0530

    Update Fuel.java

--- Oops/src/abstraction/Fuel.java
+++ Oops/src/abstraction/Fuel.java
@@ -3,7 +3,7 @@ package abstraction;
 public abstract class Fuel {              //abstract class
        abstract public void petrol();            //incomplete method
         
-        public void ready()//
+        public void ready()
         {
         	System.out.println("ready to go");
         }
