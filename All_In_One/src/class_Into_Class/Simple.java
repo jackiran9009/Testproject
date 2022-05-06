1661c65fe130a5fd4edb20be17d758b741790158
kiran9009 <103761487+kiran9009@users.noreply.github.com>
2022-05-06 13:18:18 +0530

    cs

--- All_In_One/src/class_Into_Class/Simple.java
+++ All_In_One/src/class_Into_Class/Simple.java
@@ -2,25 +2,27 @@ package class_Into_Class;
 
 public class Simple {
 	public static void main(String[] args) {
-		//		Samp s=new Samp();
-		//		s.show1();
+		//		
 		Samp.show2();
+	}
+	public void show1()
+	{
+		System.out.println("it is show1");
 	}
 	public static void show()
 	{
 		System.out.println("it is show");
 	}
 	public class Samp{
-		public void show1()
-		{
-			System.out.println("it is show1");
-		}
+	
 		public static void show2()
 		{
 			System.out.println("it is show2");
 		}
 		public static void main(String[] args) {
 			Simple.show();
+			Simple  s=new Simple ();
+					s.show1();
 		}
 	}
 }
---
+++ simple/src/logical/hkjhjk.java
@@ -0,0 +1,10 @@
+package logical;
+
+public class hkjhjk {
+   public static void main(String[] args) {
+	
+
+	String s="hitesh";
+   System.out.println(s.toCharArray());
+   }   
+}
